c31c98f7ef434c8fce184a5b153ea417faf04d5b
John Hirakawa <johnhirakawa@Johns-MacBook-Pro.local>
2015-06-29 23:01:41 -0400

    Initial Commit

---
+++ SpaceMan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpaceMan
+//
+//  Created by John Hirakawa on 6/29/15.
+//  Copyright (c) 2015 John Hirakawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpaceMan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SpaceMan
+//
+//  Created by John Hirakawa on 6/29/15.
+//  Copyright (c) 2015 John Hirakawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
